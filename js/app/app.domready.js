1604a237918521e92fb84ca47fa4f7c67338a59b
Christopher Marshal <thoughtpalettechris@gmail.com>
2012-07-19 09:49:53 -0500

    Cleaned up Files
    Removed Raptorize files. While a cool plugin, not a Rec.
    Deleted mailcheck.js
    Put Selectivzr in IE Conditional statement.
    Removed jQuery 1.7 from lib.
    Removed references to js from domReady.js

--- js/app/app.domready.js
+++ js/app/app.domready.js
@@ -10,32 +10,6 @@ $(document).ready(
 	    	}
 		});
 
-
-		//MailCheck - https://github.com/Kicksend/mailcheck
-
-		 var domains = ['hotmail.com', 'gmail.com', 'aol.com'];
-
-	    $('input#email').blur(function(){
-	          $('input#email').mailcheck(domains, {
-	            suggested: function(element, suggestion) {
-					  address: 'test',         // the address; part before the @ sign
-					  domain: 'hotmail.com',   // the suggested domain
-					  full: 'test@hotmail.com',  // the full suggested email
-
-	              $('#emailSuggestion').html("did you mean <a id='suggestion' >"+suggestion.full+"</a>?");      
-	            },
-	            empty: function(element) {
-	                $('#emailSuggestion').html("please add you email address");
-	            }
-	          });
-	    });
-
-	    //this is click handler if user clicks on the mailcheck suggestion
-	    $(document.body).delegate('#suggestion', 'click', function(){
-	        $('input#email_address').val(this.innerHTML);
-	        $('#emailSuggestion').empty();
-	    });
-
 		$('.phoneUS').mask('(999) 999-9999');
 		
 		$('[placeholder]').setPlaceholderText();
